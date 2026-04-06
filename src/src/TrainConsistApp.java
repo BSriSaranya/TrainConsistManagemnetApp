d406ca7eca45d2c711b5ab59da0e2aec90fced15
BSriSaranya <sb6595@srmist.edu.in>
2026-04-06 15:03:57 +0530

    UC17

--- src/src/TrainConsistApp.java
+++ src/src/TrainConsistApp.java
@@ -1,42 +1,14 @@
 package src;
 
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String msg) {
-        super(msg);
-    }
-}
-
-class GoodsBogie {
-    String type;
-    String cargo;
-
-    GoodsBogie(String type) {
-        this.type = type;
-    }
-
-    void assignCargo(String cargo) {
-        try {
-            if (type.equals("Rectangular") && cargo.equals("Petroleum"))
-                throw new CargoSafetyException("Unsafe cargo assignment");
-            this.cargo = cargo;
-            System.out.println("Cargo assigned: " + cargo);
-        } catch (CargoSafetyException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Assignment attempt completed");
-        }
-    }
-}
+import java.util.*;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        String[] bogies = {"Sleeper","AC Chair","First Class","General","Luxury"};
 
-        b1.assignCargo("Petroleum");   // safe
-        b2.assignCargo("Petroleum");   // unsafe
+        Arrays.sort(bogies);
 
-        System.out.println("Program continues...");
+        System.out.println(Arrays.toString(bogies));
     }
 }
