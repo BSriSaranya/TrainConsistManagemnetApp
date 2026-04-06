3964fb5c28c1e9d683c8fd684cdea404d1426ffa
BSriSaranya <sb6595@srmist.edu.in>
2026-04-06 14:50:27 +0530

    UC14

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,26 +1,31 @@
 package src;
 
-import java.util.*;
+class InvalidCapacityException extends Exception {
+    public InvalidCapacityException(String msg) {
+        super(msg);
+    }
+}
 
-class Bogie {
-    String type, cargo;
-    Bogie(String type, String cargo) {
+class PassengerBogie {
+    String type;
+    int capacity;
+
+    PassengerBogie(String type, int capacity) throws InvalidCapacityException {
+        if (capacity <= 0)
+            throw new InvalidCapacityException("Capacity must be greater than zero");
         this.type = type;
-        this.cargo = cargo;
+        this.capacity = capacity;
     }
 }
 
 public class TrainConsistApp {
     public static void main(String[] args) {
-        List<Bogie> list = Arrays.asList(
-                new Bogie("Cylindrical", "Petroleum"),
-                new Bogie("Open", "Coal"),
-                new Bogie("Box", "Grain")
-        );
-
-        boolean safe = list.stream()
-                .allMatch(b -> !b.type.equals("Cylindrical") || b.cargo.equals("Petroleum"));
-
-        System.out.println(safe ? "Train is Safe" : "Train is Unsafe");
+        try {
+            PassengerBogie b1 = new PassengerBogie("Sleeper", 72);
+            PassengerBogie b2 = new PassengerBogie("AC", -10); // invalid
+            System.out.println("Bogie Created");
+        } catch (InvalidCapacityException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
